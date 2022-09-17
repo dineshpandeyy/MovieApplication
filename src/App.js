15dc898c591c78abcb1aafa4a7407e6639f6b2d4
Dinesh Pandey <dineshpandey3536@gmail.com>
2022-09-16 20:57:21 -0500

    added input field for search functionality

--- src/App.js
+++ src/App.js
@@ -2,26 +2,35 @@ import './App.css';
 import React, {useEffect, useState} from 'react';
 import MovieList from './components/MovieList';
 import "bootstrap/dist/css/bootstrap.min.css";
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox'; 
 
 
 const App = ()=>  {
   const [movies, setMovies] = useState([ ]);
+  const [searchValue, setSearchValue] = useState('');
 
-const getMovieRequest = async () => {
-  const url = "http://www.omdbapi.com/?s=avengers&apikey=308e0de5"
+const getMovieRequest = async (searchValue) => {
+  const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=308e0de5`;
 
   const response = await fetch(url);
   const responseJson = await response.json();
   console.log(responseJson);
-  setMovies(responseJson.Search);
+  if(responseJson.Search){
+    setMovies(responseJson.Search);
+  }
 };
 
 useEffect(()=>{
-  getMovieRequest()
-}, []);
+  getMovieRequest(searchValue);
+}, [searchValue]);
 
   return (
     <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies'/>
+        <SearchBox  searchValue = {searchValue} setSearchValue={setSearchValue}/>
+      </div>
       <div className="row">
       <MovieList movies = {movies} />
       </div>
