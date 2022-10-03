12546588c32118ad4469335fc296fa30b20643d4
Dinesh Pandey <dineshpandey3536@gmail.com>
2022-10-03 17:51:41 -0500

    added hover effect in img container

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,10 +3,15 @@ import React from 'react';
 const MovieList = (props) => {
     return (
         <>
-        {props.movies.map((movie, index)=> 
-        <div className="d-flex justify-content-start m-3">
+        {props.movies.map((movie, index)=> (
+        <div className="img-container d-flex justify-content-start m-3">
             <img src= {movie.Poster} alt="movie"></img>
-        </div>)}
+            <div className='overlay d-flex align-items-center justify-content-center'>
+
+            </div>
+
+
+        </div>))}
         </>
     )
 }
